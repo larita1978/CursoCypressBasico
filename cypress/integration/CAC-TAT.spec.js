94b24c57f67be9f25e74f0888ecef6de5ba495ed
Larissa <larissainesdemorais@hotmail.com>
2023-06-13 23:51:02 -0300

    exercicio extra 2 - valida se msg de erro é exibida

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,7 +15,7 @@ describe('Central e Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function(){
         cy.get('#firstName').type('Fulano')
         cy.get('#lastName').type('da Silva')        
         cy.get('#email').type('Fulano@gmail.com')
@@ -27,5 +27,17 @@ describe('Central e Atendimento ao Cliente TAT', function() {
             .should('be.visible')
             //.should('be.equal','Mensagem enviada com sucesso.')
         
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Fulano')
+        cy.get('#lastName').type('da Silva')        
+        cy.get('#email').type('Fulanogmail.com')
+        cy.get('#open-text-area').type('Teste se apresenta mensagem de erro', { delay : 0})
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error')
+            .should('be.visible')
     })
 })
